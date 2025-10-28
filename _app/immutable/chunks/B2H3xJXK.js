b120b924dcc4ec6706420d43b966ae4d9629f2a9
rafistrauss <rafistrauss@users.noreply.github.com>
2025-10-28 16:23:23 +0000

    deploy: b64db9a34a5ab12e9c5bf6475a1127ee7877bc4b

---
+++ _app/immutable/chunks/B2H3xJXK.js
@@ -0,0 +1,1 @@
+var s;const e=((s=globalThis.__sveltekit_rcot09)==null?void 0:s.base)??"/budget";var t;const a=((t=globalThis.__sveltekit_rcot09)==null?void 0:t.assets)??e;export{a,e as b};
---
+++ _app/immutable/entry/start.CBhdaLh8.js
@@ -0,0 +1,1 @@
+import{l as o,a as r}from"../chunks/McHg1J4K.js";export{o as load_css,r as start};
